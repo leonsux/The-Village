7fcb9f7908e808677d951028665973369e032a57
leonsuxgit config --global user.email leonsux@163.com <leonsux@163.com>
2018-04-10 23:27:42 +0800

    添加点击补番的动画效果

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -84,8 +84,7 @@ function showcomics(results) {
         $(this).removeClass('tada');
     })
     // 加入购物车
-    $('.caption').delegate('.add-cart', 'click', function () {
-        console.log("舒服舒服")
+    $('.caption').delegate('.add-cart', 'click', function (e) {
         // 获取存在cookie中的用户信息，如果存在则获取，否则给个空值
         var user_info = $.cookie('user_info') ? JSON.parse($.cookie('user_info')) : null
 
@@ -104,6 +103,31 @@ function showcomics(results) {
             alert("先去登陆吧！")
             window.location.href = '/login'
         }
+        var $elem = document.createElement("b");
+        $elem.style.color = '#' + Math.floor(Math.random() * 0xffffff).toString(16); //"#E94F06";
+        $elem.style.zIndex = 9999;
+        $elem.style.position = "absolute";
+        $elem.style.select = "none";
+        var x = e.pageX;
+        var y = e.pageY;
+        $elem.style.left = (x - 10) + "px";
+        $elem.style.top = (y - 20) + "px";
+        clearInterval(anim);
+        $elem.innerText = "❤";
+        $elem.style.fontSize = Math.random() * 10 + 12 + "px";
+        var increase = 0;
+        var anim;
+        setTimeout(function() {
+          anim = setInterval(function() {
+              if (++increase == 150) {
+                  clearInterval(anim);
+                    document.body.removeChild($elem);
+              }
+              $elem.style.top = y - 20 - increase + "px";
+              $elem.style.opacity = (150 - increase) / 120;
+          }, 8);
+        }, 70);
+        document.body.appendChild($elem);
     })
     $(".search").click(function(){
         window.open('https://www.baidu.com/s?wd=' + $(this).data('name'))
